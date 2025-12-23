507289e5f61f6b891dcc71fc29f8b4bf4333bb84
Manuel Salazar <mannisuso@gmail.com>
2025-12-23 12:38:30 -0500

    feat: add docstrings to each of the routes

--- backend/app/routers/supermarket.py
+++ backend/app/routers/supermarket.py
@@ -13,20 +13,24 @@ router = APIRouter(
     tags=["supermarkets"],
 )
 
+
 @router.post("/", response_model=SupermarketRead, status_code=status.HTTP_201_CREATED)
 def create_supermarket(*, session: Session = Depends(get_session), supermarket_in: SupermarketCreate):
+    """Create a new supermarket"""
     supermarket = crud_supermarket.create_supermarket(session=session, supermarket_in=supermarket_in)
     return supermarket
 
 
 @router.get("/", response_model=List[SupermarketRead])
 def read_supermarkets(session: Session = Depends(get_session), skip: int = 0, limit: int = 100):
+    """Get all supermarkets"""
     supermarkets = crud_supermarket.get_supermarkets(session=session, skip=skip, limit=limit)
     return supermarkets
 
 
 @router.get("/{supermarket_id}", response_model=SupermarketRead)
 def read_supermarket(*, session: Session = Depends(get_session), supermarket_id: int):
+    """Get a specific supermarket by ID"""
     supermarket = crud_supermarket.get_supermarket(session=session, supermarket_id=supermarket_id)
     if not supermarket:
         raise HTTPException(status_code=404, detail="Supermarket not found")
@@ -35,6 +39,7 @@ def read_supermarket(*, session: Session = Depends(get_session), supermarket_id:
 
 @router.put("/{supermarket_id}", response_model=SupermarketRead)
 def update_supermarket(*, session: Session = Depends(get_session), supermarket_id: int, supermarket_in: SupermarketUpdate):
+    """Update a supermarket"""
     supermarket = crud_supermarket.get_supermarket(session=session, supermarket_id=supermarket_id)
     if not supermarket:
         raise HTTPException(status_code=404, detail="Supermarket not found")
@@ -44,6 +49,7 @@ def update_supermarket(*, session: Session = Depends(get_session), supermarket_i
 
 @router.delete("/{supermarket_id}", response_model=SupermarketRead)
 def delete_supermarket(*, session: Session = Depends(get_session), supermarket_id: int):
+    """Delete a supermarket"""
     supermarket = crud_supermarket.delete_supermarket(session=session, supermarket_id=supermarket_id)
     if not supermarket:
         raise HTTPException(status_code=404, detail="Supermarket not found")
